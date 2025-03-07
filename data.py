283248c9e2b37e4d4660a36d81c9503d1b8a0658
Elayabarathi <elayabarathiedison@gmail.com>
2025-03-07 22:27:01 +0530

    third commit

--- data.py
+++ data.py
@@ -1,14 +1,11 @@
-
 import requests
 
-params = {
+parameters = {
     "amount": 10,
     "type": "boolean",
 }
 
-response = requests.get(url="https://opentdb.com/api.php", params=params)
+response = requests.get("https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
 data = response.json()
 question_data = data["results"]
-
-
